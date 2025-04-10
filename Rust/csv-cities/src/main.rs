2ea42305accf986ee766b6dd6f1abb5b7d200b2e
ben05allen <ben05allen@hotmail.com>
2025-04-10 12:03:42 +0900

    starting rust version

---
+++ Rust/csv-cities/src/main.rs
@@ -0,0 +1,28 @@
+use std::{env, error::Error, ffi::OsString, process};
+
+type Record = (String, String, Option<u64>, f64, f64);
+
+fn main() {
+    if let Err(err) = run() {
+        println!("{}", err);
+        process::exit(1);
+    }
+}
+
+fn run() -> Result<(), Box<dyn Error>> {
+    let file_path = get_first_arg()?;
+    let mut rdr = csv::Reader::from_path(file_path)?;
+
+    for result in rdr.deserialize() {
+        let record: Record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
+fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
+    match env::args_os().nth(1) {
+        None => Err(From::from("expected 1 argument, but got none")),
+        Some(file_path) => Ok(file_path),
+    }
+}
