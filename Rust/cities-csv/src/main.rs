f04b10fa37c281bb24c3aab7126d823bcd84c0cb
ben05allen <ben05allen@hotmail.com>
2025-04-10 21:26:34 +0900

    Rust solution, a cross between Python and Go

--- Rust/cities-csv/src/main.rs
+++ Rust/cities-csv/src/main.rs
@@ -1,39 +1,81 @@
+use csv::Reader;
 use serde::Deserialize;
-use std::{env, error::Error, ffi::OsString, process};
+use sqlx::sqlite::SqlitePoolOptions;
+use std::{error::Error, path::Path};
+use tokio::sync::mpsc;
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "PascalCase")]
-struct Record {
+struct City {
     #[serde(rename = "City")]
     name: String,
     latitude: f64,
     longitude: f64,
     #[serde(deserialize_with = "csv::invalid_option")]
-    population: Option<u64>,
+    population: Option<u32>,
     state: String,
 }
 
-fn main() {
-    if let Err(err) = run() {
-        println!("{}", err);
-        process::exit(1);
-    }
-}
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let csv_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("cities.csv");
+    let db_path = format!(
+        "sqlite://{}",
+        Path::new(env!("CARGO_MANIFEST_DIR"))
+            .join("cities.db")
+            .display()
+    );
 
-fn run() -> Result<(), Box<dyn Error>> {
-    let file_path = get_first_arg()?;
-    let mut rdr = csv::Reader::from_path(file_path)?;
+    let pool = SqlitePoolOptions::new()
+        .max_connections(5)
+        .connect(&db_path)
+        .await?;
+
+    sqlx::query(
+        "CREATE TABLE IF NOT EXISTS cities (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                state TEXT NOT NULL,
+                population INTEGER,
+                latitude REAL NOT NULL,
+                longitude REAL NOT NULL
+            );",
+    )
+    .execute(&pool)
+    .await?;
+
+    let mut rdr = Reader::from_path(&csv_path)?;
+    let (tx, mut rx) = mpsc::channel::<City>(100);
+
+    let db_handle = tokio::spawn({
+        let pool = pool.clone();
+        async move {
+            while let Some(city) = rx.recv().await {
+                sqlx::query(
+                    "INSERT INTO cities (name, state, population, latitude, longitude) 
+                    VALUES (?, ?, ?, ?, ?);",
+                )
+                .bind(&city.name)
+                .bind(&city.state)
+                .bind(city.population)
+                .bind(city.latitude)
+                .bind(city.longitude)
+                .execute(&pool)
+                .await
+                .unwrap();
+            }
+        }
+    });
 
     for result in rdr.deserialize() {
-        let record: Record = result?;
-        println!("{:?}", record);
+        let city: City = result?;
+        tx.send(city).await?;
     }
-    Ok(())
-}
 
-fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
-    match env::args_os().nth(1) {
-        None => Err(From::from("expected 1 argument, but got none")),
-        Some(file_path) => Ok(file_path),
-    }
+    drop(tx);
+    db_handle.await?;
+
+    pool.close().await;
+    println!("Data written to database");
+    Ok(())
 }
